6fe182982ffb783ca81c84ad95fda19c9428cee1
Aidan Stewart <aidanstewart1529@protonmail.com>
2019-12-16 04:34:10 +0900

    Update SiderealTimeCalculator.py
    Cleanup

--- SiderealTimeCalculator.py
+++ SiderealTimeCalculator.py
@@ -6,61 +6,51 @@
 #References:
 #http://aa.usno.navy.mil/faq/docs/JD_Formula.php
 #http://aa.usno.navy.mil/faq/docs/GAST.php and
+class SiderealTime:
+    longitude = 0
+    date_of_birth = datetime.now()
 
-Long = -72.866      #Longitude of location in question (BMX LAT = 40.869 [40deg 52' 8"], BMX LONG = -72.866 [-72deg 51' 57"], Custer LONG = -72.435)
+    # input('Date of birth in format (month-day-year hour:min:second am/pm) ')
+    def __init__(self, date_time, longitude):
+        self.date_of_birth = datetime.strptime(date_time, '%m-%d-%Y %H:%M')
+        self.longitude = longitude
+        print(date_of_birth)
 
+    # Longitude of location in question (BMX LAT = 40.869 [40deg 52' 8"], BMX LONG = -72.866 [-72deg 51' 57"], Custer LONG = -72.435)
 
-#Calculate longitude in DegHHMM format for edification of user:
-hemisphere = 'W'
-if Long > 0:        #if the number is positive it's in the Eastern hemisphere
-    hemisphere = 'E'
-LongDeg = int(Long)
-LongMin = (Long - int(Long))*60
-LongSec = (LongMin - int(LongMin))*60
-LongMin = int(LongMin)
-LongSec = int(LongSec)
+    # split TD into individual variables for month, day, etc. and convert to floats:
+    MM = float(date_of_birth.month)
+    DD = float(date_of_birth.day)
+    YY = float(date_of_birth.year)
+    YY = YY + 2000
+    hh = float(date_of_birth.hour)
+    mm = float(date_of_birth.minute)
+    # convert mm to fractional time:
+    mm = mm / 60
+    # reformat UTC time as fractional hours:
+    UT = hh + mm
 
-print '\n\n\nThe longitude is set to %sdeg, [%s %sdeg %s\' %s\"]' %(Long, hemisphere, LongDeg, LongMin, LongSec)
-TD = raw_input('\nEnter the UTC time and date as MMDDYY HHMM. (UTC = EST+5, EDT+4):\n')
+    # calculate the Julian date:
+    def julian_date(self):
+        return (367 * self.YY) - int((7 * (self.YY + int((self.MM + 9) / 12))) / 4) + int((275 * self.MM) / 9) + self.DD + \
+               1721013.5 + (self.UT / 24)
 
-#split TD into individual variables for month, day, etc. and convert to floats:
-MM = float(TD[0:2])
-DD = float(TD[2:4])
-YY = float(TD[4:6])
-YY = YY+2000
-hh = float(TD[7:9])
-mm = float(TD[9:11])
+    # calculate the Greenwhich mean sidereal time:
+    def greenwhich_mean_sidereal_rile(self):
+        GMST = 18.697374558 + 24.06570982441908 * (self.julian_date() - 2451545)
+        GMST = GMST % 24  # use modulo operator to convert to 24 hours
+        GMSTmm = (GMST - int(GMST)) * 60  # convert fraction hours to minutes
+        GMSTss = (GMSTmm - int(GMSTmm)) * 60  # convert fractional minutes to seconds
+        return int(GMST), int(GMSTmm), int(GMSTss)
 
-#convert mm to fractional time:
-mm = mm/60
-
-#reformat UTC time as fractional hours:
-UT = hh+mm
-
-#calculate the Julian date:
-JD = (367*YY) - int((7*(YY+int((MM+9)/12)))/4) + int((275*MM)/9) + DD + 1721013.5 + (UT/24)
-print '\nJulian Date: JD%s' %(JD)
-
-#calculate the Greenwhich mean sidereal time:
-GMST = 18.697374558 + 24.06570982441908*(JD - 2451545)
-GMST = GMST % 24    #use modulo operator to convert to 24 hours
-GMSTmm = (GMST - int(GMST))*60          #convert fraction hours to minutes
-GMSTss = (GMSTmm - int(GMSTmm))*60      #convert fractional minutes to seconds
-GMSThh = int(GMST)
-GMSTmm = int(GMSTmm)
-GMSTss = int(GMSTss)
-print '\nGreenwhich Mean Sidereal Time: %s:%s:%s' %(GMSThh, GMSTmm, GMSTss)
-
-#Convert to the local sidereal time by adding the longitude (in hours) from the GMST.
-#(Hours = Degrees/15, Degrees = Hours*15)
-Long = Long/15      #Convert longitude to hours
-LST = GMST+Long     #Fraction LST. If negative we want to add 24...
-if LST < 0:
-    LST = LST +24
-LSTmm = (LST - int(LST))*60          #convert fraction hours to minutes
-LSTss = (LSTmm - int(LSTmm))*60      #convert fractional minutes to seconds
-LSThh = int(LST)
-LSTmm = int(LSTmm)
-LSTss = int(LSTss)
-
-print '\nLocal Sidereal Time %s:%s:%s \n\n' %(LSThh, LSTmm, LSTss)
+    # Convert to the local sidereal time by adding the longitude (in hours) from the GMST.
+    # (Hours = Degrees/15, Degrees = Hours*15)
+    def local_sidereal_time(self):
+        GMST, GMSTMM, GMSTSS = self.greenwhich_mean_sidereal_rile()
+        longitude = self.longitude / 15  # Convert longitude to hours
+        LST = GMST + longitude  # Fraction LST. If negative we want to add 24...
+        if LST < 0:
+            LST = LST + 24
+        LSTmm = (LST - int(LST)) * 60  # convert fraction hours to minutes
+        LSTss = (LSTmm - int(LSTmm)) * 60  # convert fractional minutes to seconds
+        return int(LST), int(LSTmm), int(LSTss),
